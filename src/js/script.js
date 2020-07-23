26d29f3103caf615279d81707f18784be917f80a
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-07-23 20:34:12 +0200

    Add products redendering

--- src/js/script.js
+++ src/js/script.js
@@ -53,10 +53,33 @@
   };
 
   class Product{
-    constructor(){
+
+    constructor(id, data){
       const thisProduct = this;
 
-      console.log('new Product:', thisProduct );
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+
     }
   }
 
@@ -67,7 +90,7 @@
       console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product (productData, thisApp.data.products[productData]);
       }
     },
 
