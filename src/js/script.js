f5309a3f62cbeda9c9e2cb16bf47fb6d30116d1a
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-07-23 19:17:00 +0200

    Add ech product instantion

--- src/js/script.js
+++ src/js/script.js
@@ -66,8 +66,9 @@
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
 
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     initData: function(){
