06eb873b2ea28b2e6ca5fd616124f785ec6a690d
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-08-26 21:52:47 +0200

    Add links to order and booking from main

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -1,10 +1,11 @@
-import {templates, select} from '../settings.js';
+import {templates, classNames, select} from '../settings.js';
 
 class Main {
   constructor(mainElement) {
     const thisMain = this;
 
     thisMain.render(mainElement);
+    this.linksToPages();
   }
 
   render(mainElement) {
@@ -15,6 +16,42 @@ class Main {
     thisMain.dom = {};
     thisMain.dom.wrapper = mainElement;
     thisMain.dom.wrapper.innerHTML = generatedHTML;
+  }
+
+  linksToPages(){
+
+    const linkToOrder = document.getElementById('page-order');
+    console.log(linkToOrder);
+    const linkToOrderId = linkToOrder.getAttribute('href').replace('#', '');
+
+
+    const linkToBooking = document.getElementById('page-booking');
+    console.log(linkToBooking);
+    const linkToBookingId = linkToBooking.getAttribute('href').replace('#', '');
+
+    
+
+    const pages = document.querySelector(select.containerOf.pages).children;
+
+    linkToOrder.addEventListener('click', function(){
+      for(let page of pages){
+        if(page.id == linkToOrderId){
+          page.classList.add(classNames.pages.active);
+        } else {
+          page.classList.remove(classNames.pages.active);
+        }
+      }
+    });
+
+    linkToBooking.addEventListener('click', function(){
+      for(let page of pages){
+        if(page.id == linkToBookingId){
+          page.classList.add(classNames.pages.active);
+        } else {
+          page.classList.remove(classNames.pages.active);
+        }
+      }
+    });
   }
 }
 
